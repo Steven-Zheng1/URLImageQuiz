c20a42054de8f1630023c3040201927416c4ecba
Steven Zheng <Zhengs7@mymail.vcu.edu>
2021-06-14 21:49:29 -0400

    update

--- src/Notes and Research/Example Quiz/main.java
+++ src/Notes and Research/Example Quiz/main.java
@@ -1,5 +1,7 @@
+import java.io.IOException;
+
 public class main {
-   public static void main(String[] args) {
+   public static void main(String[] args) throws IOException {
        quiz qz = new quiz();
 
    }
