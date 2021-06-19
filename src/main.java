d87d56b56509b145713f63c057f53aca76e5a1cd
Steven-Zheng1 <80347036+Steven-Zheng1@users.noreply.github.com>
2021-06-18 23:40:03 -0400

    Adding banner text

--- src/main.java
+++ src/main.java
@@ -1,2 +1,8 @@
+/************************************************************************************
+ * Authors: Steven Zheng, Shaun King
+ * Date: 6/18/2021
+ * Description: This is where the object of the program is being initialized and run from
+ ***************************************************************************************/
+
 public class main {
 }
